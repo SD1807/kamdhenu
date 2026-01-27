988dfebab227c83fee750136c73e973441fff0e1
Test User <test@example.com>
2026-01-27 11:27:50 +0530

    Fix: Add missing storage export in firebase.js

--- Internalsoftware-main/src/firebase.js
+++ Internalsoftware-main/src/firebase.js
@@ -1,7 +1,6 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
